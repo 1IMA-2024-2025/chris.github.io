ab3d8699b89b6396a4c205a28f01be10e9166fb6
Minecat300 <chrisdanriviere@gmail.com>
2024-10-25 11:31:55 +0200

    update

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,19 +3,22 @@ const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 
 var MainImg = document.getElementById("MainImage");
-let text = "url(";
+let text = 'url("';
 
 const Send  = async (id) => {
     var button = document.getElementById(id);
     button.disabled = true;
-    //for (let i = 0; i < 3; i++) {
 
-    //    window.alert(button.type);
-        MainImg.style.backgroundImage = text.concat(button.name, ")");
-        await sleep(100);
-    //}
-    
+    MainImg.style.backgroundImage = text.concat(button.name, '")');
+    button.style.border = "solid rgb(15, 32, 59) 10px" 
+    await sleep(100);
+ 
     button.disabled = false;
+
+    while (MainImg.style.backgroundImage == text.concat(button.name, '")')) {
+        await sleep(30)
+    }
+    button.style.border = "solid rgb(2, 15, 40) 10px" 
 }
 
 function Click(id) {
