3b25905bcf596bd29bce03cc403c2cad0c5e30ed
Minecat300 <chrisdanriviere@gmail.com>
2024-10-29 14:22:29 +0100

    update

---
+++ docs/js/script.js
@@ -0,0 +1,33 @@
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
+var background = document.getElementById("main")
+let height = screen.height
+let backheight = background.scrollHeight
+let state = false
+
+function FGtick() {
+    height = screen.height
+    backheight = background.scrollHeight
+    console.log("screen heigh: " + height)
+    console.log("div height: " + backheight)
+    if (state) {
+        if (backheight < height) {
+            state = false
+            background.style.height = "100vh"
+        }
+    } else {
+        if (backheight > height+10) {
+            state = true
+            background.style.height = "100%"
+        }
+    }
+}
+
+const fixBackground  = async () => {
+    while (true) {
+        FGtick()
+        await sleep(100)
+    }
+}
+
+fixBackground()
